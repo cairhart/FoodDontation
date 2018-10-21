7e82a416b917347f71e393bda2efb8e3effeb7ec
Liliana Terry <lilianaterry@gmail.com>
2018-10-21 10:36:29 -0500

    basic button style

--- food-money-donation/src/App.js
+++ food-money-donation/src/App.js
@@ -13,6 +13,16 @@ const style_button = {
     padding: '0 30px',
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
   },
+  bigButton: {
+    marginRight: 50,
+    background: "rgba(0, 0, 0, 0.5)",
+    borderRadius: 3,
+    border: 50,
+    color: "white",
+    height: 100,
+    width: 150,
+    padding: "0 30px"
+  }
 };
 
 const BasicExample = () => (
