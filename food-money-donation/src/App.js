8a1ab2cb7391fe598dcb90325fc47ec816d50e85
Carter Airhart <airhartcarter@utexas.edu>
2018-10-21 10:14:43 -0500

    Test :

--- food-money-donation/src/App.js
+++ food-money-donation/src/App.js
@@ -1,28 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Button from 'material-ui/Button';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
 
-export default App;
+// We can inject some CSS into the DOM.
+const style_button = {
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  },
+};
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Landing} />
+      <Route path="/donorlanding" component={DonorLanding} />
+      <Route path="/financialdonation" component={FinancialDonation} />
+      <Route path="/thankyou" component={ThankYou} />
+      <Route path="/fooddonation" component={FoodDonation} />
+      <Route path="/vendorlanding" component={VendorLanding} />
+      <Route path="/vendorsignup" component={VendorSignup} />
+      <Route path="/vendorinfo" component={VendorInfo} />
+    </div>
+  </Router>
+);
+
+const Landing = () => (
+  <div>
+    <Button style={style_button}>
+     ahk
+    </Button>
+  </div>
+);
+
+const DonorLanding = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const FinancialDonation = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const ThankYou = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const FoodDonation = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const VendorLanding = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const VendorSignup = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const VendorInfo = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+export default BasicExample;
